f270a62ce7bde2ced1f55bb0fa220e78b5f94abc
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-08-25 14:42:06 -0400

    Add function to take data and write it to a file; removed some blocks of comments.

--- app.js
+++ app.js
@@ -24,9 +24,6 @@ const requireEmail = (value) => {
     return 'Please enter a valid email address.'
 }
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
 // An array of questions to be used with inquirer
 const questions = [
     {
@@ -80,6 +77,14 @@ const questions = [
     },
 ];
 
+// Takes data and writes it to a file called index.html in the output folder.
+const writeToFile = (data) => {
+    fs.writeFile('./output/index.html', data, (err) => {
+        if (err) console.log(err);
+        else console.log("File generated.");
+    });
+}
+
 // Get answers from the prompt questions
 const promptUser = async () => {
     const answers = await inquirer.prompt(questions);
@@ -90,28 +95,13 @@ const promptUser = async () => {
     // If the last question, to add another employee, is true, prompt the user with questions again
     if (answers.continue) {
         promptUser();
+    // Otherwise use the array of employee classes to generate the html file
+    } else {
+        // Calls function from render module to create the html file
+        const html = render(employees);
+        writeToFile(html);
     }
 }
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
 
 // Function to create new class instances and push to employees
 const newEmployee = (answers) => {
@@ -143,8 +133,6 @@ const newEmployee = (answers) => {
 // Function to initialize the program
 const init = () => {
     promptUser();
-
-
 };
 
 init();
