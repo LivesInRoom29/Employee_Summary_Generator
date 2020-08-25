733ea01e238b3a553ef2cf532a34b95fcb6e9198
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-08-25 16:41:31 -0400

    Move questions for require and email validation function to separate modules

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+const questions = require('./lib/questions');
+
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
@@ -12,70 +14,6 @@ const render = require("./lib/htmlRenderer");
 
 // Array to store employee information
 const employees = [];
-
-// Regex for email validation modified from: https://www.regular-expressions.info/email.html
-// This function will be passed to validate the email input
-const requireEmail = (value) => {
-    const pattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,63}$/;
-    // Return true if the value matches the regex pattern
-    if (pattern.test(value)) {
-        return true;
-    }
-    return 'Please enter a valid email address.'
-}
-
-// An array of questions to be used with inquirer
-const questions = [
-    {
-        type: 'input',
-        name: 'name',
-        message: 'What is the name of the employee?'
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'What is the employee\'s ID number?'
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is the employee\'s email address?',
-        validate: requireEmail,
-    },
-    {
-        type: 'list',
-        name: 'employeetype',
-        message: 'What is this employee\'s role?',
-        choices: [
-            'manager',
-            'engineer',
-            'intern',
-        ]
-    },
-    {
-        type: 'input',
-        name: 'officeNumber',
-        message: 'What is the manager\'s office number?',
-        when: (answers) => answers.employeetype === 'manager',
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'What is the engineer\'s github username?',
-        when: (answers) => answers.employeetype === 'engineer',
-    },
-    {
-        type: 'input',
-        name: 'school',
-        message: 'What school does the intern attend?',
-        when: (answers) => answers.employeetype === 'intern',
-    },
-    {
-        type: 'confirm',
-        name: 'continue',
-        message: 'Would you like to add another employee?',
-    },
-];
 
 // Takes data and writes it to a file called index.html in the output folder.
 const writeToFile = (data) => {
